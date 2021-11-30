6fa862bbfb0af309f982f1e41463e469137883b0
arianajansma <ajansma@zagmail.gonzaga.edu>
2021-11-29 16:25:34 -0800

    added some xml

--- app/src/main/java/com/example/outdoorpartners/MainActivity.java
+++ app/src/main/java/com/example/outdoorpartners/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.outdoorpartners;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    static final String TAG = "MainActivityTag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
     }
 }
+
+// recycler view
