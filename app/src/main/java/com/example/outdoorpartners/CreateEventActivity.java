445bea96fae2852ebdf1fe640408e052850004cb
dpmacisaac <dmacisaac@zagmail.gonzaga.edu>
2021-11-29 17:24:31 -0800

    time picker working

--- app/src/main/java/com/example/outdoorpartners/CreateEventActivity.java
+++ app/src/main/java/com/example/outdoorpartners/CreateEventActivity.java
@@ -2,6 +2,7 @@ package com.example.outdoorpartners;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
@@ -11,12 +12,12 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.timepicker.TimeFormat;
 
-import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.TimeZone;
 
@@ -34,7 +35,12 @@ public class CreateEventActivity extends AppCompatActivity {
 
 
     String eventName;
-    DateFormat eventDate;
+    int eventDay;
+    int eventMonth;
+    int eventYear;
+    int eventMin;
+    int eventHour;
+
     TimeFormat eventTime;
     String eventType;
     String description;
@@ -42,11 +48,6 @@ public class CreateEventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_event_activity);
-
-
-
-
-
 
         editTextEventName = findViewById(R.id.editTextEventName);
         buttonDate = findViewById(R.id.buttonSetDate);
@@ -69,39 +70,74 @@ public class CreateEventActivity extends AppCompatActivity {
             }
         });
 
-        MyEditTextDatePicker datePicker = new MyEditTextDatePicker(this,R.id.buttonSetDate);
+        MyDatePicker datePicker = new MyDatePicker(this,R.id.buttonSetDate);
 
+        MyTimePicker timePicker = new MyTimePicker(this,R.id.buttonSetTime);
 
     }
 
+    public class MyTimePicker implements View.OnClickListener, TimePickerDialog.OnTimeSetListener{
 
-    public class MyEditTextDatePicker  implements View.OnClickListener, DatePickerDialog.OnDateSetListener {
         Button buttonText;
-        private int _day;
-        private int _month;
-        private int _year;
-        private Context _context;
+        Context context;
 
-        public MyEditTextDatePicker(Context context, int editTextViewID)
+        public MyTimePicker(Context context, int editButtonTextId) {
+            Activity act = (Activity)context;
+            this.buttonText = (Button) act.findViewById(editButtonTextId);
+            this.buttonText.setOnClickListener(this);
+            this.context = context;
+        }
+
+        @Override
+        public void onTimeSet(TimePicker timePicker, int i, int i1) {
+            eventHour = i;
+            eventMin = i1;
+            updateDisplay();
+        }
+
+        @Override
+        public void onClick(View view) {
+            TimePickerDialog tpd = new TimePickerDialog(context,this,eventHour,eventMin,false);
+            tpd.show();
+        }
+        private void updateDisplay(){
+            if(eventHour>12) {
+                buttonTime.setText(new StringBuilder()
+                        // Month is 0 based so add 1
+                        .append(eventHour % 12).append(":").append(eventMin).append(" PM"));
+            }
+            else{
+                buttonTime.setText(new StringBuilder()
+                        // Month is 0 based so add 1
+                        .append(eventHour).append(":").append(eventMin).append(" AM"));
+            }
+        }
+    }
+
+    public class MyDatePicker implements View.OnClickListener, DatePickerDialog.OnDateSetListener {
+        Button buttonText;
+        private Context context;
+
+        public MyDatePicker(Context context, int editButtonTextId)
         {
             Activity act = (Activity)context;
-            this.buttonText = (Button) act.findViewById(editTextViewID);
+            this.buttonText = (Button) act.findViewById(editButtonTextId);
             this.buttonText.setOnClickListener(this);
-            this._context = context;
+            this.context = context;
         }
 
         @Override
         public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-            _year = year;
-            _month = monthOfYear;
-            _day = dayOfMonth;
+            eventYear = year;
+            eventMonth = monthOfYear;
+            eventDay = dayOfMonth;
             updateDisplay();
         }
         @Override
         public void onClick(View v) {
             Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
 
-            DatePickerDialog dialog = new DatePickerDialog(_context, this,
+            DatePickerDialog dialog = new DatePickerDialog(context, this,
                     calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
                     calendar.get(Calendar.DAY_OF_MONTH));
             dialog.show();
@@ -113,7 +149,7 @@ public class CreateEventActivity extends AppCompatActivity {
 
             buttonDate.setText(new StringBuilder()
                     // Month is 0 based so add 1
-                    .append(_day).append("/").append(_month + 1).append("/").append(_year).append(" "));
+                    .append(eventMonth+1).append("/").append(eventDay).append("/").append(eventYear).append(" "));
         }
     }
 }
